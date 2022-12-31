6816365ebeb88ef4ebd27abcce22fd64e3ef02ee
janes <janes.presence@gmail.com>
2022-12-31 22:32:58 +0530

    first commit

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ app.post("/failure", function(req, res){
     res.redirect("/");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running on the port 3000");
 })
